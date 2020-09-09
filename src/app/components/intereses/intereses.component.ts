6563b6ffa77ab24b253c6f8917ea1c5a1d205110
Max <mgerdts@formax.cl>
2020-09-09 19:40:03 -0300

    ajustes

--- src/app/components/intereses/intereses.component.ts
+++ src/app/components/intereses/intereses.component.ts
@@ -8,31 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class InteresesComponent implements OnInit {
 
   intereses:any = [{
-    image:'test',
+    image:'../assets/Image 1.png',
     titulo:'Sabes que son los datos abiertos?',
     desc:'La información que producen las entidades públicas a tu alcance',
     linkText:'Me interesa!',
     link:'#'
   },
   {
-    image:'test',
-    titulo:'Sabes que son los datos abiertos?',
-    desc:'La información que producen las entidades públicas a tu alcance',
-    linkText:'Me interesa!',
+    image:'../assets/colombiaco_Mesa de trabajo 1.png',
+    titulo:'Conoce más sobre nuestro país',
+    desc:'Colombia es hermosa, y queremos que conozcas más sobre ella',
+    linkText:'Ir a Colombia.co',
     link:'#'
   },
   {
     image:'test',
-    titulo:'Sabes que son los datos abiertos?',
-    desc:'La información que producen las entidades públicas a tu alcance',
-    linkText:'Me interesa!',
+    titulo:'Este portal está pensando para ti',
+    desc:'GOV.CO nace para facilitarle a los ciudadanos la interacción con el Estado',
+    linkText:'Quiero saber como funciona',
     link:'#'
   },
   {
     image:'test',
-    titulo:'Sabes que son los datos abiertos?',
-    desc:'La información que producen las entidades públicas a tu alcance',
-    linkText:'Me interesa!',
+    titulo:'Destacado que esté en acondicionado ',
+    desc:'Texto descriptivo que capte la atención del lector',
+    linkText:'Call to action',
     link:'#'
   }]
 
